7cd6ef3d6135451cbecae944713b44300a88e905
Antonio Carlos Pereira Neto <133928083+netoacpn@users.noreply.github.com>
2023-11-23 23:31:48 -0300

    Adiciona Exercicio01

---
+++ src/main/java/tech/devinhouse/m02s10/M02s10Application.java
@@ -0,0 +1,13 @@
+package tech.devinhouse.m02s10;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class M02s10Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(M02s10Application.class, args);
+	}
+
+}
